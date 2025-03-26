7bef5935bde27aa813b582469eab997196bdee2d
Robin Sundman Nilsson <sundmanrobin@outlook.com>
2025-03-26 16:43:55 +0100

    09_security

---
+++ 09_security/user_login.py
@@ -0,0 +1,18 @@
+from connect_postgres import connect_postgres
+from sqlalchemy import text
+
+username = input("Enter your username: ")
+password = input("Enter you password: ")
+
+with connect_postgres().connect() as conn:
+    result = conn.execute(
+        text(
+            f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
+        )
+    ).fetchall()
+
+    if result:
+        print("Login succesful")
+        print("Eat all ice cream!")
+    else:
+        print("Login failed")
